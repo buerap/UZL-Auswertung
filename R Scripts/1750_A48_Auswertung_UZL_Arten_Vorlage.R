e12265194c4feb47f8d02c016a2246a85357a00e
buerap <raphaelsandro.vb@gmail.com>
2020-10-13 16:06:22 +0200

    - Notizen zu Codes hinzufügen - Code anpassen, verbessern - Individuen pro Arten berechnen

--- R Scripts/1750_A48_Auswertung_UZL_Arten_Vorlage.R
+++ R Scripts/1750_A48_Auswertung_UZL_Arten_Vorlage.R
@@ -9,11 +9,11 @@ library(Rmisc)
 library(ggthemes)
 library(readxl)
 library(devtools)
-install_github("TobiasRoth/BDM")
-library(BDM)
+#install_github("TobiasRoth/BDM")
+#library(BDM)
 
 # Connection to data base
-db <- src_sqlite(path = "DB/DB_BDM_2020_08_20.db", create = FALSE)
+db <- src_sqlite(path = "database/DB_BDM_2020_08_20.db", create = FALSE)
 
 # Plot Einstellungen
 theme_set(
@@ -24,84 +24,197 @@ theme_set(
       legend.background = element_rect(colour = "white"))
 )
 
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Write function - eher nicht ----
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+entwicklung <- function(y            = "Arten", 
+                        Kopfdaten    = "KD_Z7",  # KD_Z7 # KD_Z9
+                        Aufnahmeyahr = "yearBu", # yearPl (Pflanzen) # yearBi (Vogel) # yearBu (Tagfalter)  
+){
+  
+}
+
+
+
+
+
+
+
+
+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #  Entwicklung Z7-Tagfalter ----
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-dat <- tbl(db, "KD_Z7") %>% 
-  filter(!is.na(yearBu)) %>%
-  filter(Aufnahmetyp == "BDM_LANAG_Normalaufnahme_Z7" | Aufnahmetyp == "Normalaufnahme_Z7") %>% 
-  left_join(tbl(db, "Raumdaten_Z7")) %>% 
-  filter(Verdichtung_BDM == "nein") %>% 
-  left_join(tbl(db, "STICHPROBE_Z7")) %>% 
-  filter(BDM_aktuell == "ja") %>% 
-  dplyr::select(aID_KD, aID_STAO, Hoehe, Aufnahmejahr = yearBu) %>% 
+# Datentabelle erstellen
+dat <- tbl(db, "KD_Z7") %>%    # Kopfdaten
+  filter(!is.na(yearBu)) %>%   # nur Aufnahmejahre miteinbeziehen
+  filter(Aufnahmetyp == "BDM_LANAG_Normalaufnahme_Z7" | Aufnahmetyp == "Normalaufnahme_Z7") %>%  # Aufnahmetyp
+  left_join(tbl(db, "Raumdaten_Z7")) %>%  # Raumdaten (z.B. Hoehe)
+  filter(Verdichtung_BDM == "nein") %>%   # verdichtete Regionen Jura und Tessin bereinigen
+  left_join(tbl(db, "STICHPROBE_Z7")) %>% # schwer zugaengliche flaechen gehoeren nicht mehr zur stichprobe
+  filter(BDM_aktuell == "ja") %>%         # dito
+  dplyr::select(aID_KD, aID_STAO, Hoehe, Aufnahmejahr = yearBu) %>% # spalten waehlen die ich brauche als flaecheninformationen
   left_join(
-    tbl(db, "TF") %>% 
-      filter(!is.na(aID_SP)) %>% 
-      left_join(tbl(db, "Arten")) %>% 
-      group_by(aID_KD) %>% 
+    tbl(db, "TF") %>%  # Tagfalteraufnahmen
+      filter(!is.na(aID_SP)) %>% # unbestimmte Arten rausfiltern
+      left_join(tbl(db, "Arten")) %>% # Artaufnahmen
+      group_by(aID_KD) %>%            # gruppiert nach Stichprobenflaechen die folgenden rechnungen durchfuehren
       dplyr::summarise(
-        AZ = n(),
-        UZL_AZ = sum(UZL == 1),
-        UB_AZ = sum(UZL == 0),
-        UZL_Ind = sum(Ind[UZL == 1]),
-        UB_Ind = sum(Ind[UZL == 0])
-      )
-  ) %>% 
+        AZ = n(),                     # Summe der Gesamt-Artenzahl (AZ)
+        AZ_UZL = sum(UZL == 1),       # Summe der UZL-Arten
+        AZ_UB = sum(UZL == 0),        # Summe der uebrigen (nicht-UZL) Arten
+        IZ = sum(Ind),                # Summe der Gesamt-Individuenzahl (IZ)
+        IZ_UZL = sum(Ind[UZL == 1]),  # Summe der UZL-Individuen
+        IZ_UB = sum(Ind[UZL == 0]),    # Summe der uebrigen (nicht-UZL) Arten
+        IZperAZ = mean(Ind),
+        IZperAZ_UZL = mean(Ind[UZL == 1]),
+        IZperAZ_UB = mean(Ind[UZL == 0]))
+  ) %>%
   as_tibble() %>% 
-  replace_na(list(UZL_Ind = 0)) # NA in Spalte UZL_Ind mit Null ersetzen (Null-Individuen auch wichtig)
-  
-# Plot machen
-dat %>% 
-  group_by(Aufnahmejahr) %>% 
+  replace_na(list(IZ_UZL = 0, IZ_UB = 0, IZ = 0, IZperAZ_UZL = 0))
+                    #%>%       # NA in Spalte UZL_Ind durch Null ersetzen
+  #add_column(IZperAZ     = dat$IZ/dat$AZ,         # bei jeder Aufnahmeflaeche wird jedes Aufnahmejahr
+   #          IZperAZ_UZL = dat$IZ_UZL/dat$AZ_UZL, # durchschnittliche Individuenzahl pro Art bestimmt
+   #          IZperAZ_UB  = dat$IZ_UB/dat$AZ_UB)   # und dies fuer: UZL, Uebrige, gesamt  
+      
+
+
+
+# Plot Artenzahl
+TF1 <- dat %>% 
+  group_by(Aufnahmejahr) %>%  # gruppiert nach Aufnahmejahr die Mittelwerte aller Flaechen berechnen
   dplyr::summarise(
-    UZL_AZ = mean(UZL_AZ),
-    UZL_UB = mean(UB_AZ)) %>% 
-  gather("Artengruppe", "AZ", -Aufnahmejahr) %>% 
+    UZL = mean(AZ_UZL),
+    übrige = mean(AZ_UB)) %>% 
+  gather("Artengruppe", "AZ", -Aufnahmejahr) %>% # umwandeln in long-format
   ggplot(aes(x = Aufnahmejahr, y = AZ, col = Artengruppe)) +
   geom_point() +
   geom_line() +
   geom_smooth(method = "lm") +
+  ggtitle("ARTENZAHL: Tagfalter (Z7)") +
   ylim(0, NA) +
-  labs(
-    x = "",
-    y = "Anzahl Arten"
-  )
+  labs(x = "Aufnahmejahr",
+       y = "Anzahl Arten") +
+  theme(legend.position = c(0.85, 0.15))
 
-# Plot machen
-dat %>% 
-  group_by(Aufnahmejahr) %>% 
+# Plot Individuenzahl relativ
+TF2 <- dat %>%
+  group_by(Aufnahmejahr) %>%
   dplyr::summarise(
-    UZL_Ind = mean(UZL_Ind) / mean(dat$UZL_Ind),
-    UB_Ind = mean(UB_Ind)/ mean(dat$UB_Ind)) %>% 
-  gather("Artengruppe", "AZ", -Aufnahmejahr) %>% 
+    UZL = mean(AZ_UZL) / mean(dat$AZ_UZL),
+    übrige = mean(AZ_UB)/ mean(dat$AZ_UB)) %>%
+  gather("Artengruppe", "AZ", -Aufnahmejahr) %>%
   ggplot(aes(x = Aufnahmejahr, y = AZ, col = Artengruppe)) +
   geom_point() +
   geom_line() +
   geom_smooth(method = "lm") +
-  ylim(0, NA) +
-  labs(
-    x = "",
-    y = "Anzahl Individuen"
-  )
+  ggtitle("ARTENZAHL relativ: Tagfalter (Z7)") +
+  ylim(0, 2) +
+  labs(x = "Aufnahmejahr",
+       y = "Arten relativ zum Durchschnitt 2003 - 2019") +
+  theme(legend.position = c(0.85, 0.15))
 
-library(arm)
-d <- dat %>% 
-  dplyr::select(aID_STAO, Jahr = Aufnahmejahr, UZL_Ind, UB_Ind) %>% 
-  gather("Artengruppe", "Ind", -c(aID_STAO, Jahr)) %>% 
-  mutate(Jahr_sd = (Jahr-2010) / 10)
-lmer(Ind ~ Jahr_sd * Artengruppe + (1|aID_STAO), data = d) %>% 
-  summary
-
-dat %>% 
-  group_by(aID_STAO) %>% 
-  dplyr::summarise(AZ = mean(AZ), Hoehe = mean(Hoehe)) %>% 
-  ggplot(aes(x = Hoehe, y = AZ)) + 
+# Plot Individuenzahl
+TF3 <- dat %>% 
+  group_by(Aufnahmejahr) %>%  # gruppiert nach Aufnahmejahr die Mittelwerte aller Flaechen berechnen
+  dplyr::summarise(
+    UZL = mean(IZ_UZL),
+    übrige = mean(IZ_UB)) %>% 
+  gather("Artengruppe", "IZ", -Aufnahmejahr) %>% # umwandeln in long-format
+  ggplot(aes(x = Aufnahmejahr, y = IZ, col = Artengruppe)) +
   geom_point() +
-  geom_smooth()
+  geom_line() +
+  geom_smooth(method = "lm") +
+  ggtitle("INDIVIDUENZAHL: Tagfalter (Z7)") +
+  ylim(0, NA) +
+  labs(x = "Aufnahmejahr",
+       y = "Anzahl Individuen") +
+  theme(legend.position = c(0.85, 0.15))
+
+# Plot Individuenzahl relativ
+TF4 <- dat %>%
+  group_by(Aufnahmejahr) %>%
+  dplyr::summarise(
+    UZL = mean(IZ_UZL) / mean(dat$IZ_UZL),
+    übrige = mean(IZ_UB)/ mean(dat$IZ_UB)) %>%
+  gather("Artengruppe", "IZ", -Aufnahmejahr) %>%
+  ggplot(aes(x = Aufnahmejahr, y = IZ, col = Artengruppe)) +
+  geom_point() +
+  geom_line() +
+  geom_smooth(method = "lm") +
+  ggtitle("INDIVIDUENZAHL relativ: Tagfalter (Z7)") +
+  ylim(0, 2) +
+  labs(x = "Aufnahmejahr",
+       y = "Individuen relativ zum Durchschnitt 2003 - 2019") +
+  theme(legend.position = c(0.85, 0.15))
+
+# Plot Individuen pro Arten
+TF5 <- dat %>% 
+  group_by(Aufnahmejahr) %>%  # gruppiert nach Aufnahmejahr die Mittelwerte aller Flaechen berechnen
+  dplyr::summarise(
+    UZL = mean(IZperAZ_UZL),
+    übrige = mean(IZperAZ_UB)) %>% 
+  gather("Artengruppe", "IZperAZ", -Aufnahmejahr) %>% # umwandeln in long-format
+  ggplot(aes(x = Aufnahmejahr, y = IZperAZ, col = Artengruppe)) +
+  geom_point() +
+  geom_line() +
+  geom_smooth(method = "lm") +
+  ggtitle("Individuen pro Arten: Tagfalter (Z7)") +
+  ylim(0, NA) +
+  labs(x = "Aufnahmejahr",
+       y = "durchschnittliche Anzahl Individuen pro Art") +
+  theme(legend.position = c(0.85, 0.15))
+
+
+
+
+# plot on same page
+gridExtra::grid.arrange(TF1, TF2, TF3, TF4, TF5, ncol = 2, nrow = 3)
+
+
+
+# library(arm)
+# d <- dat %>% 
+#   dplyr::select(aID_STAO, Jahr = Aufnahmejahr, UZL_Ind, UB_Ind) %>% 
+#   gather("Artengruppe", "Ind", -c(aID_STAO, Jahr)) %>% 
+#   mutate(Jahr_sd = (Jahr-2010) / 10)
+# lmer(Ind ~ Jahr_sd * Artengruppe + (1|aID_STAO), data = d) %>% 
+#   summary
+# 
+# dat %>% 
+#   group_by(aID_STAO) %>% 
+#   dplyr::summarise(AZ = mean(AZ), Hoehe = mean(Hoehe)) %>% 
+#   ggplot(aes(x = Hoehe, y = AZ)) + 
+#   geom_point() +
+#   geom_smooth()
   
 
+
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Entwicklung Z7-Voegel ----
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Entwicklung Z7-Pflanzen ----
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Entwicklung Z9-Pflanzen ----
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Entwicklung Z9-Moose ----
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Entwicklung Z9-Mollusken ----
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
