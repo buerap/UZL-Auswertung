4b95fa7eff9e71b95176122e45dadf269b256403
buerap <raphaelsandro.vb@gmail.com>
2020-12-11 17:29:21 +0100

    Update

--- R Scripts/1750_A48_Auswertung_UZL_Arten_REPORT.R
+++ R Scripts/1750_A48_Auswertung_UZL_Arten_REPORT.R
@@ -15,7 +15,7 @@ cat( "\014" )                     # Clear Console ( =  CTRL L)
 library(tidyverse)
 library(ggthemes)
 library(scales)
-#library(Rmisc)
+library(Rmisc)
 #library(readxl)
 #library(devtools)
 #install_github("TobiasRoth/BDM")
@@ -34,7 +34,7 @@ theme_set(             # ggplot theme()-default Einstellungen
 db <- src_sqlite(path = "database/DB_BDM.db", create = FALSE)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## Vorbereitung Daten (HIER LOOP SCHREIBEN)----
+## Vorbereitung Daten (HIER evtl. einige löschen / NICHT als Loop schreiben)----
 # Pflanzen Z7
 PL.Z7 <- tbl(db, "KD_Z7") %>%   
   filter(!is.na(yearPl)) %>%   
@@ -57,7 +57,7 @@ PL.Z7_Artaufnahmen <- tbl(db, "KD_Z7") %>%
   left_join(tbl(db, "PL") %>%
               filter(!is.na(aID_SP)) %>%
               left_join(tbl(db, "Arten"))) %>%
-    dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearPl, Hoehe, AntWald, AntLW, AntSiedlung, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL)
+    dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearPl, Hoehe, AntWald, AntLW, AntSiedlung, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL, Z7Z9)
 
 # Tagfalter Z7
 TF.Z7 <- tbl(db, "KD_Z7") %>%   
@@ -81,7 +81,7 @@ TF.Z7_Artaufnahmen <- tbl(db, "KD_Z7") %>%
   left_join(tbl(db, "TF") %>%
               filter(!is.na(aID_SP)) %>%
               left_join(tbl(db, "Arten"))) %>%
-  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearBu, Hoehe, AntWald, AntLW, AntSiedlung, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL)
+  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearBu, Hoehe, AntWald, AntLW, AntSiedlung, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL, Z7Z9)
 
 # Voegel Z7
 BI.Z7 <- tbl(db, "KD_Z7") %>%   
@@ -105,7 +105,7 @@ BI.Z7_Artaufnahmen <- tbl(db, "KD_Z7") %>%
   left_join(tbl(db, "BI") %>%
               filter(!is.na(aID_SP)) %>%
               left_join(tbl(db, "Arten"))) %>%
-  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearBi, Hoehe, AntWald, AntLW, AntSiedlung, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL)
+  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearBi, Hoehe, AntWald, AntLW, AntSiedlung, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL, Z7Z9)
 
 
 # Mollusken Z9
@@ -128,7 +128,7 @@ MOL.Z9_Artaufnahmen <- tbl(db, "KD_Z9") %>%
   left_join(tbl(db, "MOL") %>%  
               filter(!is.na(aID_SP)) %>%  
               left_join(tbl(db, "Arten"))) %>% 
-  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearMol, Hoehe, HN, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL)
+  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearMol, Hoehe, HN, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL, Z7Z9)
 
 # Moose Z9
 MOOS.Z9 <- tbl(db, "KD_Z9") %>%      
@@ -150,7 +150,7 @@ MOOS.Z9_Artaufnahmen <- tbl(db, "KD_Z9") %>%
   left_join(tbl(db, "MOOS") %>%  
               filter(!is.na(aID_SP)) %>%  
               left_join(tbl(db, "Arten"))) %>% 
-  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearMoos, Hoehe, HN, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL)
+  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearMoos, Hoehe, HN, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL, Z7Z9)
 
 # Pflanzen Z9
 PL.Z9 <- tbl(db, "KD_Z9") %>%      
@@ -172,7 +172,7 @@ PL.Z9_Artaufnahmen <- tbl(db, "KD_Z9") %>%
   left_join(tbl(db, "PL") %>%  
               filter(!is.na(aID_SP)) %>%  
               left_join(tbl(db, "Arten"))) %>% 
-  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearPl, Hoehe, HN, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL)
+  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearPl, Hoehe, HN, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL, Z7Z9)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## Deskriptiv: Beschreibung BDM Aufnahmen ----
@@ -279,7 +279,7 @@ PL.Z7_relativ <- PL.Z7_Aufnahmen %>%
 PL.Z7_relativ
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## relativ andere Artengruppen ----
+## relativ andere Artengruppen (alles in einen Plot machen) ----
 # Tagfalter Z7
 TF.Z7_Aufnahmen <- TF.Z7 %>% 
   left_join(
@@ -310,68 +310,260 @@ TF.Z7_relativ <- TF.Z7_Aufnahmen %>%
   scale_y_continuous(labels=percent)
 TF.Z7_relativ
 
+# Vögel Z7
+BI.Z7_Aufnahmen <- BI.Z7 %>% 
+  left_join(
+    tbl(db, "BI") %>%  
+      filter(!is.na(aID_SP)) %>% 
+      left_join(tbl(db, "Arten")) %>% 
+      group_by(aID_KD) %>%
+      dplyr::summarise(AZ = n(),
+                       AZ_UZL = sum(UZL == 1),
+                       AZ_UB = sum(UZL == 0))
+  ) %>% as_tibble() %>% 
+  replace_na(list(AZ = 0, AZ_UZL = 0, AZ_UB = 0)) %>% print() 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-PL.Z7 <- tbl(db, "KD_Z7") %>%   
-  filter(!is.na(yearPl)) %>%   
-  filter(yearPl<2020) %>% 
-  filter(Aufnahmetyp == "BDM_LANAG_Normalaufnahme_Z7" | Aufnahmetyp == "Normalaufnahme_Z7") %>%  
-  left_join(tbl(db, "Raumdaten_Z7")) %>%  
-  filter(Verdichtung_BDM == "nein") %>%   
-  left_join(tbl(db, "STICHPROBE_Z7")) %>% 
-  filter(BDM_aktuell == "ja") %>% 
-  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearPl, Hoehe, AntWald, AntLW, AntSiedlung)
-dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearBu, Hoehe, AntWald, AntLW, AntSiedlung, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL)
-# left_join(tbl(db, "PL") %>%  
-#             filter(!is.na(aID_SP)) %>%  
-#             left_join(tbl(db, "Arten"))) %>% 
-#dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearPl, Hoehe, AntWald, AntLW, AntSiedlung, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL) %>% 
-#as_tibble()
-
-
-
+BI.Z7_relativ <- BI.Z7_Aufnahmen %>% 
+  group_by(Aufnahmejahr) %>%
+  dplyr::summarise(UZL    = (mean(AZ_UZL) / mean(BI.Z7_Aufnahmen$AZ_UZL)) - 1,
+                   übrige = (mean(AZ_UB)  / mean(BI.Z7_Aufnahmen$AZ_UB))  - 1) %>% 
+  gather("Artengruppe", "AZ", -Aufnahmejahr) %>% 
+  ggplot(aes(x = Aufnahmejahr, y = AZ, col = Artengruppe)) +
+  geom_point() +
+  geom_line() +
+  geom_smooth(method = "lm") +
+  ggtitle("ARTENZAHL relativ: Vögel (Z7)") +
+  labs(x = "Aufnahmejahr",
+       y = "Veränderte Artenzahl relativ zum Durchschnitt 2003 - 2019") +
+  theme(legend.position = c(0.85, 0.15))+
+  scale_y_continuous(labels=percent)
+BI.Z7_relativ
 
 # Mollusken Z9
-MOL.Z9 <- tbl(db, "KD_Z9") %>%      
-  filter(!is.na(yearMol)) %>%   
-  filter(yearMol<2020) %>% 
-  filter(Aufnahmetyp == "BDM_LANAG_Normalaufnahme_Z9" | Aufnahmetyp == "Normalaufnahme_Z9") %>%  
-  left_join(tbl(db, "Raumdaten_Z9")) %>%  
-  left_join(tbl(db, "STICHPROBE_Z9")) %>% 
-  filter(BDM_aktuell == "ja") %>%        
-  dplyr::select(aID_KD, aID_STAO, Aufnahmejahr = yearMol, Hoehe, HN, aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL)
+MOL.Z9_Aufnahmen <- MOL.Z9 %>% 
+  left_join(
+    tbl(db, "MOL") %>%  
+      filter(!is.na(aID_SP)) %>% 
+      left_join(tbl(db, "Arten")) %>% 
+      group_by(aID_KD) %>%
+      dplyr::summarise(AZ = n(),
+                       AZ_UZL = sum(UZL == 1),
+                       AZ_UB = sum(UZL == 0))
+  ) %>% as_tibble() %>% 
+  replace_na(list(AZ = 0, AZ_UZL = 0, AZ_UB = 0)) %>% print() 
+
+MOL.Z9_relativ <- MOL.Z9_Aufnahmen %>% 
+  group_by(Aufnahmejahr) %>%
+  dplyr::summarise(UZL    = (mean(AZ_UZL) / mean(MOL.Z9_Aufnahmen$AZ_UZL)) - 1,
+                   übrige = (mean(AZ_UB)  / mean(MOL.Z9_Aufnahmen$AZ_UB))  - 1) %>% 
+  gather("Artengruppe", "AZ", -Aufnahmejahr) %>% 
+  ggplot(aes(x = Aufnahmejahr, y = AZ, col = Artengruppe)) +
+  geom_point() +
+  geom_line() +
+  geom_smooth(method = "lm") +
+  ggtitle("ARTENZAHL relativ: Mollusken (Z9)") +
+  labs(x = "Aufnahmejahr",
+       y = "Veränderte Artenzahl relativ zum Durchschnitt 2003 - 2019") +
+  theme(legend.position = c(0.85, 0.15))+
+  scale_y_continuous(labels=percent)
+MOL.Z9_relativ
+
+# Moose Z9
+MOOS.Z9_Aufnahmen <- MOOS.Z9 %>% 
+  left_join(
+    tbl(db, "MOOS") %>%  
+      filter(!is.na(aID_SP)) %>% 
+      left_join(tbl(db, "Arten")) %>% 
+      group_by(aID_KD) %>%
+      dplyr::summarise(AZ = n(),
+                       AZ_UZL = sum(UZL == 1),
+                       AZ_UB = sum(UZL == 0))
+  ) %>% as_tibble() %>% 
+  replace_na(list(AZ = 0, AZ_UZL = 0, AZ_UB = 0)) %>% print() 
+
+MOOS.Z9_relativ <- MOOS.Z9_Aufnahmen %>% 
+  group_by(Aufnahmejahr) %>%
+  dplyr::summarise(UZL    = (mean(AZ_UZL) / mean(MOOS.Z9_Aufnahmen$AZ_UZL)) - 1,
+                   übrige = (mean(AZ_UB)  / mean(MOOS.Z9_Aufnahmen$AZ_UB))  - 1) %>% 
+  gather("Artengruppe", "AZ", -Aufnahmejahr) %>% 
+  ggplot(aes(x = Aufnahmejahr, y = AZ, col = Artengruppe)) +
+  geom_point() +
+  geom_line() +
+  geom_smooth(method = "lm") +
+  ggtitle("ARTENZAHL relativ: Moose (Z9)") +
+  labs(x = "Aufnahmejahr",
+       y = "Veränderte Artenzahl relativ zum Durchschnitt 2003 - 2019") +
+  theme(legend.position = c(0.85, 0.15))+
+  scale_y_continuous(labels=percent)
+MOOS.Z9_relativ
+
+# Pflanzen Z9
+PL.Z9_Aufnahmen <- PL.Z9 %>% 
+  left_join(
+    tbl(db, "PL") %>%  
+      filter(!is.na(aID_SP)) %>% 
+      left_join(tbl(db, "Arten")) %>% 
+      group_by(aID_KD) %>%
+      dplyr::summarise(AZ = n(),
+                       AZ_UZL = sum(UZL == 1),
+                       AZ_UB = sum(UZL == 0))
+  ) %>% as_tibble() %>% 
+  replace_na(list(AZ = 0, AZ_UZL = 0, AZ_UB = 0)) %>% print() 
+
+PL.Z9_relativ <- PL.Z9_Aufnahmen %>% 
+  group_by(Aufnahmejahr) %>%
+  dplyr::summarise(UZL    = (mean(AZ_UZL) / mean(PL.Z9_Aufnahmen$AZ_UZL)) - 1,
+                   übrige = (mean(AZ_UB)  / mean(PL.Z9_Aufnahmen$AZ_UB))  - 1) %>% 
+  gather("Artengruppe", "AZ", -Aufnahmejahr) %>% 
+  ggplot(aes(x = Aufnahmejahr, y = AZ, col = Artengruppe)) +
+  geom_point() +
+  geom_line() +
+  geom_smooth(method = "lm") +
+  ggtitle("ARTENZAHL relativ: Pflanzen (Z9)") +
+  labs(x = "Aufnahmejahr",
+       y = "Veränderte Artenzahl relativ zum Durchschnitt 2003 - 2019") +
+  theme(legend.position = c(0.85, 0.15))+
+  scale_y_continuous(labels=percent)
+PL.Z9_relativ
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Übersichtsplot alle 6 (Entwicklung relativ) ----
+gridExtra::grid.arrange(PL.Z7_relativ,
+                        TF.Z7_relativ,
+                        BI.Z7_relativ,
+                        MOL.Z9_relativ,
+                        MOOS.Z9_relativ,
+                        PL.Z9_relativ,
+                        ncol = 2, nrow = 3, as.table = F)
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Trend vs. Landwirtschaft Z7 (NOCH NICHT FERTIG !!!) (AntLW > 20 % -> bei LW < 10% gibts komplett anderes Bild: UZL werden schlecht geschätzt --> mit Tobi Besprechen)----
+trend.rel <- function(y, x, mean_value){  # glm mit poisson weil zähldaten, aber achtung wenn relativ !! -> wenn hier mit gaussian, dann gibts bei Z9 schönere resultate
+  coef(glm(y ~ x, family = "gaussian"))[2]/(ifelse(mean_value == 0, 1, mean_value))  # wenn keine (z.B. UZL-)Arten vorkommen bei einem Standort bei allen Aufnahmejahren, wird Trend als Null angenommen (0 / 1)
+}
+
+# Pflanzen Z7
+Trend_PL.Z7 <- PL.Z7_Aufnahmen %>%
+  #filter(AntLW > 0.2) %>% 
+  group_by(aID_STAO) %>%
+  dplyr::summarise(Trend_UZL   = trend.rel(y = AZ_UZL, x = Aufnahmejahr, mean_value = mean(AZ_UZL)),
+                   Trend_UB    = trend.rel(y = AZ_UB,  x = Aufnahmejahr, mean_value = mean(AZ_UB) ),
+                   Landw       = mean(AntLW),
+                   Wald        = mean(AntWald),
+                   Siedlung    = mean(AntSiedlung),
+                   Hoehe       = mean(Hoehe)) %>%
+  dplyr::rename(UZL = Trend_UZL, UB = Trend_UB) %>% 
+  reshape2::melt(measure.vars = c("UZL", "UB")) %>% 
+  dplyr::rename(Artengruppe = variable, Trend = value) %>% 
+  as_tibble()
+
+  ggplot(Trend_PL.Z7, aes(x = Landw, y = Trend, col = Artengruppe)) +
+    #geom_point() +
+    geom_smooth(method = "lm") +
+    ggtitle("Entwicklung vs. Landwirtschaft : Pflanzen (Z7)") +
+  scale_y_continuous(labels = percent)
+  
+
+mod <- lm(Trend ~ Artengruppe*Landw + Hoehe + I(Hoehe^2) + Wald, data = Trend_PL.Z7)
+summary(mod)
+
+mod2 <- lm(Trend ~ Artengruppe*Landw, data = Trend_PL.Z7)
+summary(mod2)
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Trend vs. Landwirtschaft Z9 (NOCH NICHT FERTIG !!!) ----
+# Pflanzen Z7
+Trend_PL.Z9 <- PL.Z9_Aufnahmen %>%
+  group_by(aID_STAO) %>%
+  dplyr::summarise(Trend_UZL   = trend.rel(y = AZ_UZL, x = Aufnahmejahr, mean_value = mean(AZ_UZL)),
+                   Trend_UB    = trend.rel(y = AZ_UB,  x = Aufnahmejahr, mean_value = mean(AZ_UB) ),
+                   HN          = HN[1],
+                   Hoehe       = mean(Hoehe)) %>%
+  dplyr::rename(UZL = Trend_UZL, UB = Trend_UB) %>% 
+  reshape2::melt(measure.vars = c("UZL", "UB")) %>% 
+  dplyr::rename(Artengruppe = variable, Trend = value) %>% 
+  as_tibble()
+Trend_PL.Z9$HN <- as_factor(Trend_PL.Z9$HN)
+
+ggdata <- Trend_PL.Z9 %>%
+  group_by(HN, Artengruppe) %>%
+  summarise(
+    CI = CI(Trend)[1]-CI(Trend)[2],
+    Trend = mean(Trend)
+  )
+ggplot(ggdata, aes(HN, Trend)) +
+  geom_pointrange(
+    aes(ymin = Trend-CI, ymax = Trend+CI, color = Artengruppe),
+    position = position_dodge(0.3)
+  ) +
+  scale_y_continuous(labels = percent)
+
+
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Trend vs. Häufigkeit der Art (Z8 Daten) ----
+trend.Occ <- function(y, x){
+  coef(glm(y ~ x, family = "gaussian"))[2]
+}
+  
+  
+PL.Z7_Occ05_09 <- PL.Z7_Artaufnahmen %>% 
+  filter(Aufnahmejahr < 2010 & Aufnahmejahr > 2004) %>% 
+  filter(Z7Z9 == 1) %>%
+  group_by(aID_SP) %>% 
+  dplyr::summarise(Anteil_Flaechen05_09 = n() / 375) 
+  
+PL.Z7_Occ10_14 <- PL.Z7_Artaufnahmen %>% 
+  filter(Aufnahmejahr < 2015 & Aufnahmejahr > 2009) %>% 
+  filter(Z7Z9 == 1) %>%
+  group_by(aID_SP) %>% 
+  dplyr::summarise(Anteil_Flaechen10_14 = n() / 375) 
+
+PL.Z7_Occ15_19 <- PL.Z7_Artaufnahmen %>% 
+  filter(Aufnahmejahr < 2020 & Aufnahmejahr > 2014) %>% 
+  filter(Z7Z9 == 1) %>%
+  group_by(aID_SP) %>% 
+  dplyr::summarise(Anteil_Flaechen15_19 = n() / 375) 
+
+PL.Z7_Occurence <- tbl(db, "Arten") %>%
+  filter(PL   == 1 & Z7Z9 == 1) %>%
+  left_join(PL.Z7_Occ05_09) %>% 
+  left_join(PL.Z7_Occ10_14) %>% 
+  left_join(PL.Z7_Occ15_19) %>%
+  dplyr::select(aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL, Anteil_Flaechen05_09, Anteil_Flaechen10_14, Anteil_Flaechen15_19) %>% 
+  as_tibble() %>% 
+  replace_na(list(Anteil_Flaechen05_09 = 0,
+                  Anteil_Flaechen10_14 = 0,
+                  Anteil_Flaechen15_19 = 0)) %>% 
+  filter(Anteil_Flaechen05_09 != 0 | Anteil_Flaechen10_14 != 0 | Anteil_Flaechen15_19 != 0)
+
+PL_Z7.Trend <- PL.Z7_Occurence %>% 
+  gather("Tranche", "Anteil_Flaechen", -c(aID_SP, Gattung, Art, ArtD, Z8, UZL, UZL_Zielart, RL)) %>% 
+  mutate(Tranchenzahl = ifelse(Tranche == "Anteil_Flaechen05_09", 2007, ifelse(Tranche == "Anteil_Flaechen10_14", 2012, 2017))) %>%
+  group_by(aID_SP) %>% 
+  dplyr::summarise(
+    Trend = trend.Occ(x = Tranchenzahl, y = Anteil_Flaechen),
+    Anteil_Flaechen_mean = mean(Anteil_Flaechen)) %>% 
+  left_join(
+    tbl(db, "Arten") %>% 
+      as_tibble() %>% 
+      dplyr::select(aID_SP, Gattung, Art, ArtD, UZL), by = "aID_SP") %>% 
+  rename(Artengruppe = UZL) %>% 
+  print()
+PL_Z7.Trend$Artengruppe <- as.factor(PL_Z7.Trend$Artengruppe)
+levels(PL_Z7.Trend$Artengruppe) <- c("UB", "UZL")
+PL_Z7.Trend
+
+ggplot(PL_Z7.Trend, aes(x = Anteil_Flaechen_mean, y = Trend, col = Artengruppe)) +
+  geom_smooth(method = "lm")
+  
+
 
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## END OF SCRIPT ----
-
 
 
 
